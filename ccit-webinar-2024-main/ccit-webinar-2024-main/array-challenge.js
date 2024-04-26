3f5f7706c2d46d2349161781db256199b13e5fb4
Ex-Mika <168150577+Ex-Mika@users.noreply.github.com>
2024-04-26 12:17:38 +0800

    Update array-challenge.js

--- ccit-webinar-2024-main/ccit-webinar-2024-main/array-challenge.js
+++ ccit-webinar-2024-main/ccit-webinar-2024-main/array-challenge.js
@@ -16,14 +16,14 @@ const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     9
 */
 //Answer:
-    console.log("Even Numbers:");
+    console.log("EVEN NUMBERS:");
     arr.forEach(num => {
         if (num % 2 === 0) {
             console.log(num);
         }
     });
     
-    console.log("Odd Numbers:");
+    console.log("ODD NUMBERS:");
     arr.forEach(num => {
         if (num % 2 !== 0) {
             console.log(num);
